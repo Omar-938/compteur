d92abdf0561d4390fa155e84d24ca514c4dacf55
Bensadia Omar <128587580+Omar-938@users.noreply.github.com>
2023-05-06 14:50:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const start = document.querySelector('#start');
+const howMany = document.querySelector('#combiendetemps');
+const compte = document.querySelector("#temps");
+const alert = document.querySelector("#alert");
+const restart = document.querySelector("#restart");
+
+
+
+function demarrerMinuteur(dureeEnSecondes) {
+    
+    let dureeEnMillisecondes = (parseInt(howMany.value) + 1) * 1000;
+  
+    
+  
+    let minuteur = setInterval(function() {
+      dureeEnMillisecondes -= 1000;
+  
+      let tempsRestantEnSecondes = (dureeEnMillisecondes) / 1000;
+  
+      compte.innerHTML = `Il te reste ${tempsRestantEnSecondes} Secondes`;
+       if(tempsRestantEnSecondes >= howMany.value * 0.66){
+        compte.style.color = "green";
+       }
+        
+       if(tempsRestantEnSecondes <= howMany.value *0.66 && tempsRestantEnSecondes > howMany.value * 0.33){
+        compte.style.color = "orange";
+      }
+       if(tempsRestantEnSecondes <= howMany.value * 0.33 ){
+        compte.style.color ="red";
+      }
+  
+       if (dureeEnMillisecondes <= -1) {
+        clearInterval(minuteur);
+        compte.innerHTML = "";
+        howMany.value = ""
+        alert.innerHTML = "Temps écoulé !! Personne Suivante !!"
+      }
+      restart.addEventListener("click",function(){
+        clearInterval(minuteur);
+        howMany.value = "";
+        compte.innerHTML = "";
+        alert.innerHTML= "";
+      })
+      
+      
+    }, 1000); 
+  }
+
+
+
+  start.addEventListener("click", function(){
+    demarrerMinuteur(howMany.value);
+    alert.innerHTML = "";
+  });
+
+ 
+
+
+    
+
+    
+
+
+
+
+
+
